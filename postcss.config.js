379e6c444284ecffd98389b33ff331241869c06c
Richard Szolár <r@richardszolar.me>
2020-08-28 18:33:12 +0200

    - Add basic configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
     plugins: [
-        require('tailwindcss')({
-            purge: {
-                enabled: true,
-                content: [
-                    './src/*.html',
-                    './src/*.js',
-                ]
-            },
-        }),
+        require('tailwindcss')('tailwind.config.js'),
         require('autoprefixer')
     ]
 };
