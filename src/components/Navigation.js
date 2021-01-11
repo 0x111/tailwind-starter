8166c04cc401d77f4f0148012033d1989fe0ba4f
DarkOutlaw101 <james@isger.co.uk>
2021-01-11 22:55:02 +0000

    Issue

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Navigation = () => {
     return (
-        <div className="sticky top-0 bg-red-500 shadow z-30">
+        <div className="sticky top-0 bg-red-500 md:bg-blue-600 shadow z-30">
             <nav className="py-5 md:py-10 flex items-center justify-between h-16 z-50 md:max-w-screen-lg mx-auto">
 
                     <a className="mx-auto md:mx-0">
-                        <h1>Hello</h1>
+                        <h1 className='h-32'>Hello</h1>
                     </a>
 
             </nav>
